f518dbd328972f57bb3cf63cb9a2550bf4451ade
Aoutnheub <dev.aoutnheub@gmail.com>
2022-03-28 13:52:38 +0300

    more vector functions removed Vector4D

--- include/vector.h
+++ include/vector.h
@@ -20,65 +20,95 @@ namespace cex {
             "Only integer and float types allowed"
         );
 
-        private:
-            T x_, y_;
-            double length_;
-            bool recal_len = true;
-
         public:
+            T x, y;
+
             Vector2D() {}
-            Vector2D(T _x, T _y) : x_(_x), y_(_y) {}
+            Vector2D(T _x, T _y) : x(_x), y(_y) {}
 
-            T x() { return x_; }
-
-            void x(T _x) { x_ = _x; recal_len = true; }
-
-            T y() { return y_; }
-
-            void y(T _y) { y_ = _y; recal_len = true; }
-
-            void xy(T _x, T _y) {
-                x_ = _x; y_ = _y;
-                recal_len = true;
+            float length() {
+                return std::sqrt(x * x + y * y);
             }
 
-            double length() {
-                if(recal_len)
-                    length_ = std::sqrt(x_ * x_ + y_ * y_);
-                recal_len = false;
+            // float angle() {}
 
-                return length_;
-            }
-
-            void normalize() {
-                double len = length();
-                x_ /= len; y_ /= len;
-                recal_len = true;
-            }
-
-            Vector2D<T> normalized() {
-                double len = length();
-                return Vector2D<T>(x_ / len, y_ / len);
-            }
+            // float angleTo(Vector2D<T> _vec) {}
 
             T dot(Vector2D<T> _vec) {
-                return x_ * _vec.x() + y_ * _vec.y();
+                return x * _vec.x + y * _vec.y;
             }
 
-            Vector2D<T> operator+(Vector2D<T> &_vec) {
-                return Vector2D<T>(x_ + _vec.x(), y_ + _vec.y());
+            T sum() { return x + y; }
+
+            Vector2D<T> normalize() {
+                double len = length();
+                return Vector2D<T>(x / len, y / len);
+            }
+
+            Vector2D<T> sin() {
+                return Vector2D<T>(std::sin(x), std::sin(y));
+            }
+
+            Vector2D<T> cos() {
+                return Vector2D<T>(std::cos(x), std::cos(y));
+            }
+
+            Vector2D<T> tan() {
+                return Vector2D<T>(std::tan(x), std::tan(y));
+            }
+
+            Vector2D<T> cot() {
+                return Vector2D<T>(1 / std::tan(x), 1 / std::tan(y));
+            }
+
+            Vector2D<T> abs() {
+                return Vector2D<T>(std::abs(x), std::abs(y));
+            }
+
+            Vector2D<T> ceil() {
+                return Vector2D<T>(std::ceil(x), std::ceil(y));
+            }
+
+            Vector2D<T> floor() {
+                return Vector2D<T>(std::floor(x), std::floor(y));
+            }
+
+            Vector2D<T> round() {
+                return Vector2D<T>(std::round(x), std::round(y));
+            }
+
+            // Vector2D<T> clamp(float _len) {}
+
+            Vector2D<T> operator+(T _val) {
+                return Vector2D<T>(x + _val, y + _val);
+            }
+
+            Vector2D<T> operator+(Vector2D<T> _vec) {
+                return Vector2D<T>(x + _vec.x, y + _vec.y);
             }
 
             Vector2D<T> operator*(T _val) {
-                return Vector2D<T>(x_ * _val, y_ * _val);
+                return Vector2D<T>(x * _val, y * _val);
+            }
+
+            Vector2D<T> operator*(Vector2D<T> _vec) {
+                return Vector2D<T>(x * _vec.x, y * _vec.y);
             }
 
             Vector2D<T> operator/(T _val) {
-                return Vector2D<T>(x_ / _val, y_ / _val);
+                return Vector2D<T>(x / _val, y / _val);
+            }
+
+            Vector2D<T> operator/(Vector2D<T> _vec) {
+                return Vector2D<T>(x / _vec.x, y / _vec.y);
             }
 
             Vector2D<T> operator%(T _val) {
-                return Vector2D<T>(x_ % _val, y_ % _val);
+                return Vector2D<T>(x % _val, y % _val);
+            }
+
+            Vector2D<T> operator%(Vector2D<T> _vec) {
+                return Vector2D<T>(x % _vec.x, y % _vec.y);
             }
     };
 
@@ -98,165 +128,99 @@ namespace cex {
             "Only integer and float types allowed"
         );
 
-        private:
-            T x_, y_, z_;
-            double length_;
-            bool recal_len = true;
-
         public:
+            T x, y, z;
+
             Vector3D() {}
-            Vector3D(T _x, T _y, T _z) : x_(_x), y_(_y), z_(_z) {}
+            Vector3D(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
 
-            T x() { return x_; }
-
-            void x(T _x) { x_ = _x; recal_len = true; }
-
-            T y() { return y_; }
-
-            void y(T _y) { y_ = _y; recal_len = true; }
-
-            T z() { return z_; }
-
-            void z(T _z) { z_ = _z; recal_len = true; }
-
-            void xyz(T _x, T _y, T _z) {
-                x_ = _x; y_ = _y; z_ = _z;
-                recal_len = true;
+            float length() {
+                return std::sqrt(x * x + y * y + z * z);
             }
 
-            double length() {
-                if(recal_len)
-                    length_ = std::sqrt(x_ * x_ + y_ * y_ + z_ * z_);
-                recal_len = false;
-
-                return length_;
-            }
-
-            void normalize() {
-                double len = length();
-                x_ /= len; y_ /= len; z_ /= len;
-                recal_len = true;
-            }
-
-            Vector3D<T> normalized() {
-                double len = length();
-                return Vector3D<T>(x_ / len, y_ / len, z_ / len);
-            }
+            // float angleTo(Vector3D<T> _vec) {}
 
             T dot(Vector3D<T> _vec) {
-                return x_ * _vec.x() + y_ * _vec.y() + z_ * _vec.z();
+                return x * _vec.x + y * _vec.y + z * _vec.z;
+            }
+
+            T sum() { return x + y + z; }
+
+            Vector3D<T> normalize() {
+                double len = length();
+                return Vector3D<T>(x / len, y / len, z / len);
+            }
+
+            Vector3D<T> sin() {
+                return Vector3D<T>(std::sin(x), std::sin(y), std::sin(z));
+            }
+
+            Vector3D<T> cos() {
+                return Vector3D<T>(std::cos(x), std::cos(y), std::cos(z));
+            }
+
+            Vector3D<T> tan() {
+                return Vector3D<T>(std::tan(x), std::tan(y), std::tan(z));
+            }
+
+            Vector3D<T> cot() {
+                return Vector3D<T>(1 / std::tan(x), 1 / std::tan(y), 1 / std::tan(z));
+            }
+
+            Vector3D<T> abs() {
+                return Vector3D<T>(std::abs(x), std::abs(y), std::abs(z));
+            }
+
+            Vector3D<T> ceil() {
+                return Vector3D<T>(std::ceil(x), std::ceil(y), std::ceil(z));
+            }
+
+            Vector3D<T> floor() {
+                return Vector3D<T>(std::floor(x), std::floor(y), std::floor(z));
+            }
+
+            Vector3D<T> round() {
+                return Vector3D<T>(std::round(x), std::round(y), std::round(z));
             }
 
             Vector3D<T> cross(Vector3D<T> _vec) {
-                Vector3D<T> vec(
-                    y_ * _vec.z() - z_ * _vec.y(),
-                    z_ * _vec.x() - x_ * _vec.z(),
-                    x_ * _vec.y() - y_ * _vec.x()
+                return Vector3D<T>(
+                    y * _vec.z - z * _vec.y,
+                    z * _vec.x - x * _vec.z,
+                    x * _vec.y - y * _vec.x
                 );
-
-                return vec;
             }
 
-            Vector3D<T> operator+(Vector3D<T> &_vec) {
-                return Vector3D<T>(x_ + _vec.x(), y_ + _vec.y(), z_ + _vec.z());
+            Vector3D<T> operator+(T _val) {
+                return Vector3D<T>(x + _val, y + _val, z + _val);
+            }
+
+            Vector3D<T> operator+(Vector3D<T> _vec) {
+                return Vector3D<T>(x + _vec.x, y + _vec.y, z + _vec.z);
             }
 
             Vector3D<T> operator*(T _val) {
-                return Vector3D<T>(x_ * _val, y_ * _val, z_ * _val);
+                return Vector3D<T>(x * _val, y * _val, z * _val);
+            }
+
+            Vector3D<T> operator*(Vector3D<T> _vec) {
+                return Vector3D<T>(x * _vec.x, y * _vec.y, z * _vec.z);
             }
 
             Vector3D<T> operator/(T _val) {
-                return Vector3D<T>(x_ / _val, y_ / _val, z_ / _val);
+                return Vector3D<T>(x / _val, y / _val, z / _val);
+            }
+
+            Vector3D<T> operator/(Vector3D<T> _vec) {
+                return Vector3D<T>(x / _vec.x, y / _vec.y, z / _vec.z);
             }
 
             Vector3D<T> operator%(T _val) {
-                return Vector3D<T>(x_ % _val, y_ % _val, z_ % _val);
-            }
-    };
-
-    template<typename T>
-    class Vector4D {
-        static_assert(
-            std::is_same<T, std::uint8_t>::value ||
-            std::is_same<T, std::int8_t>::value ||
-            std::is_same<T, std::uint16_t>::value ||
-            std::is_same<T, std::int16_t>::value ||
-            std::is_same<T, std::uint32_t>::value ||
-            std::is_same<T, std::int32_t>::value ||
-            std::is_same<T, std::uint64_t>::value ||
-            std::is_same<T, std::int64_t>::value ||
-            std::is_same<T, float>::value ||
-            std::is_same<T, double>::value,
-            "Only integer and float types allowed"
-        );
-
-        private:
-            T x_, y_, z_, w_;
-            double length_;
-            bool recal_len = true;
-
-        public:
-            Vector4D() {}
-            Vector4D(T _x, T _y, T _z, T _w) : x_(_x), y_(_y), z_(_z), w_(_w) {}
-
-            T x() { return x_; }
-
-            void x(T _x) { x_ = _x; recal_len = true; }
-
-            T y() { return y_; }
-
-            void y(T _y) { y_ = _y; recal_len = true; }
-
-            T z() { return z_; }
-
-            void z(T _z) { z_ = _z; recal_len = true; }
-
-            T w() { return w_; }
-
-            void w(T _w) { w_ = _w; recal_len = true; }
-
-            void xyzw(T _x, T _y, T _z, T _w) {
-                x_ = _x; y_ = _y; z_ = _z; w_ = _w;
-                recal_len = true;
+                return Vector3D<T>(x % _val, y % _val, z % _val);
             }
 
-            double length() {
-                if(recal_len)
-                    length_ = std::sqrt(x_ * x_ + y_ * y_ + z_ * z_ + w_ * w_);
-                recal_len = false;
-
-                return length_;
-            }
-
-            void normalize() {
-                double len = length();
-                x_ /= len; y_ /= len; z_ /= len; w_ /= len;
-                recal_len = true;
-            }
-
-            Vector3D<T> normalized() {
-                double len = length();
-                return Vector3D<T>(x_ / len, y_ / len, z_ / len, w_ / len);
-            }
-
-            T dot(Vector4D<T> _vec) {
-                return x_ * _vec.x() + y_ * _vec.y() + z_ * _vec.z() + w_ * _vec.w();
-            }
-
-            Vector4D<T> operator+(Vector3D<T> &_vec) {
-                return Vector4D<T>(x_ + _vec.x(), y_ + _vec.y(), z_ + _vec.z(), w_ + _vec.w());
-            }
-
-            Vector4D<T> operator*(T _val) {
-                return Vector4D<T>(x_ * _val, y_ * _val, z_ * _val, w_ * _val);
-            }
-
-            Vector4D<T> operator/(T _val) {
-                return Vector4D<T>(x_ / _val, y_ / _val, z_ / _val, w_ / _val);
-            }
-
-            Vector4D<T> operator%(T _val) {
-                return Vector4D<T>(x_ % _val, y_ % _val, z_ % _val, w_ % _val);
+            Vector3D<T> operator%(Vector3D<T> _vec) {
+                return Vector3D<T>(x % _vec.x, y % _vec.y, z % _vec.z);
             }
     };
 }
